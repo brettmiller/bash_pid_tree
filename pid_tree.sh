7c9265859306bc85690952e01f4b74b2059dd452
Brett Miller <brett.miller@ingramcontent.com>
2016-08-05 15:54:34 -0500

    working - initial cmd w/o args

---
+++ pid_tree.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Display the parent process tree for a given process
+
+M_PPID=$(sudo ss --packet -apn | grep users: | cut -d, -f2)
+#M_PPID=$1
+
+COUNT=0
+
+OIFS=$IFS
+#IFS=";"
+while [ "$M_PPID" != "1" ] ; do
+  #WPID_INFO=($( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= ))
+  WPID_INFO="$( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= )"
+  #WPID_INFO="$( ps -p "$M_PPID" --no-headers -o "%p;%P;%a;%u")"
+  PID_INFO=(${WPID_INFO})
+  M_PPID="${PID_INFO[1]}"
+  PROC_LIST[${COUNT}]="${PID_INFO[3]},${PID_INFO[0]},${PID_INFO[2]}"
+  let COUNT=$COUNT+1
+done
+IFS=$OIFS
+
+PROC_TREE=''
+while [ $COUNT -ge 0 ] ; do
+  PROC_TREE="${PROC_TREE}${PROC_LIST[${COUNT}]} ->"
+  let COUNT=$COUNT-1
+done
+
+# Strip leading and trailing '->'
+PROC_TREE=$(echo $PROC_TREE | sed -e 's/^->\(.*\)->$/\1/')
+echo "$PROC_TREE"
