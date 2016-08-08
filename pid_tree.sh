1e3eff6223ee39abfae310529bd689288dcde73a
Brett Miller <brett.miller@ingramcontent.com>
2016-08-08 14:19:37 -0500

    Make it a function for easy reuse elsewhere.

--- pid_tree.sh
+++ pid_tree.sh
@@ -4,25 +4,28 @@
 
 M_PPID=$1
 
-COUNT=0
+pidtree () {
+  COUNT=0
+  while [ "$M_PPID" != "1" ] ; do
+    #WPID_INFO=($( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= ))
+    WPID_INFO="$( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= )"
+    #WPID_INFO="$( ps -p "$M_PPID" --no-headers -o "%p;%P;%a;%u")"
+    PID_INFO=(${WPID_INFO})
+    M_PPID="${PID_INFO[1]}"
+    PROC_LIST[${COUNT}]="${PID_INFO[3]},${PID_INFO[0]},${PID_INFO[2]}"
+    let COUNT=$COUNT+1
+  done
 
-while [ "$M_PPID" != "1" ] ; do
-  #WPID_INFO=($( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= ))
-  WPID_INFO="$( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= )"
-  #WPID_INFO="$( ps -p "$M_PPID" --no-headers -o "%p;%P;%a;%u")"
-  PID_INFO=(${WPID_INFO})
-  M_PPID="${PID_INFO[1]}"
-  PROC_LIST[${COUNT}]="${PID_INFO[3]},${PID_INFO[0]},${PID_INFO[2]}"
-  let COUNT=$COUNT+1
-done
+  PID_TREE=''
+  while [ $COUNT -ge 0 ] ; do
+    PID_TREE="${PID_TREE}${PROC_LIST[${COUNT}]} ->"
+    let COUNT=$COUNT-1
+  done
 
-PID_TREE=''
-while [ $COUNT -ge 0 ] ; do
-  PID_TREE="${PID_TREE}${PROC_LIST[${COUNT}]} ->"
-  let COUNT=$COUNT-1
-done
+  # Strip leading and trailing '->'
+  PID_TREE=${PID_TREE# ->}
+  PID_TREE=${PID_TREE%->}
+  echo "$PID_TREE"
+}
 
-# Strip leading and trailing '->'
-PID_TREE=${PID_TREE# ->}
-PID_TREE=${PID_TREE%->}
-echo "$PID_TREE"
+pidtree
