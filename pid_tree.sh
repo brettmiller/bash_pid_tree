c2d335a63c9f012164f26bc4478dd0ec0bdb930c
Brett Miller <brett.miller@ingramcontent.com>
2016-08-08 16:14:47 -0500

    Deal with PID not existing. Comment code

--- pid_tree.sh
+++ pid_tree.sh
@@ -6,25 +6,33 @@
 
 pidtree () {
   M_PPID=$1
-  COUNT=0
-  while [ "$M_PPID" != "1" ] ; do
-    WPID_INFO="$( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= )"
-    PID_INFO=(${WPID_INFO})
-    M_PPID="${PID_INFO[1]}"
-    PROC_LIST[${COUNT}]="${PID_INFO[3]},${PID_INFO[0]},${PID_INFO[2]}"
-    let COUNT=$COUNT+1
-  done
+    # make sure pid exists before getting parents
+    if ps --no-header --pid $M_PPID > /dev/null 2>&1; then
+    COUNT=0
+    while [ "$M_PPID" != "1" ] ; do
+      # get pid, parent pid (ppid), user owning pid, and command name
+      WPID_INFO="$( ps -p $M_PPID -o pid= -o ppid= -o ruser= -o cmd= )"
+      # convert into array
+      PID_INFO=(${WPID_INFO})
+      # set to parent pid for next loop.
+      M_PPID="${PID_INFO[1]}"
+      PROC_LIST[${COUNT}]="${PID_INFO[3]},${PID_INFO[0]},${PID_INFO[2]}"
+      let COUNT=$COUNT+1
+    done
 
-  PID_TREE=''
-  while [ $COUNT -ge 0 ] ; do
-    PID_TREE="${PID_TREE}${PROC_LIST[${COUNT}]} ->"
-    let COUNT=$COUNT-1
-  done
+    PID_TREE=''
+    while [ $COUNT -ge 0 ] ; do
+      PID_TREE="${PID_TREE}${PROC_LIST[${COUNT}]} ->"
+      let COUNT=$COUNT-1
+    done
 
-  # Strip leading and trailing '->'
-  PID_TREE=${PID_TREE# ->}
-  PID_TREE=${PID_TREE%->}
-  echo "$PID_TREE"
+    # Strip leading and trailing '->'
+    PID_TREE=${PID_TREE# ->}
+    PID_TREE=${PID_TREE%->}
+    echo "$PID_TREE"
+  else
+    1>&2 echo "${M_PPID:=PID} not found."
+  fi
 }
 
 pidtree $1
